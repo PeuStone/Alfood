a641aa3438d077283127674477ebc04741f3ffd8
PeuStone <pedrohmotaaa@gmail.com>
2023-08-24 10:37:46 -0300

    Começando edição de restaurantes no projeto

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,20 +1,41 @@
 import { Button, TextField } from "@mui/material"
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const FormularioRestaurante = () => {
+
+    const parametros = useParams()
+
+    useEffect(() => {
+        if(parametros.id){
+            axios.get(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+                .then(resposta => setNomeRestaurante(resposta.data.nome))
+        }
+    }, [parametros])
 
     const [nomeRestaurante, setNomeRestaurante] = useState('');
 
     const aoSubmitForm = (evento: React.FormEvent<HTMLFormElement>) => {
         evento.preventDefault();
 
-        axios.post('http://localhost:8000/api/v2/restaurantes/', {
-            nome: nomeRestaurante
-        })
-            .then(() => {
-                alert("restaurante cadastrado com sucesso!")
+        if (parametros.id) {
+            axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+                nome: nomeRestaurante
             })
+                .then(() => {
+                    alert("restaurante atualizado com sucesso!")
+                })
+        } else {
+            axios.post('http://localhost:8000/api/v2/restaurantes/', {
+                nome: nomeRestaurante
+            })
+                .then(() => {
+                    alert("restaurante cadastrado com sucesso!")
+                })
+        }
+
+      
     }
 
     return (
